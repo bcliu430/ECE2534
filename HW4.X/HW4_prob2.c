48532745e5f4996c5cd463da224f95f48be6de98
ateasepeople <abc452709061@gmail.com>
2016-10-18 22:20:56 -0400

    Update HW4_prob2.c

--- HW4.X/HW4_prob2.c
+++ HW4.X/HW4_prob2.c
@@ -5,6 +5,19 @@
 //                  The example follows the five-step  "Grandma's ISR recipe"
 //                  as introduced in class.
 //
+//                  The timer ISR is also used to update an LED using a 
+//                  simple PWM (look it up!) scheme. The LED update is done
+//                  in separate files (a definition and implementation).
+//
+//                  The program uses the OLED to display a counter that updates
+//                  every NUMBER_OF_MILLISECONDS_PER_OLED_UPDATE milliseconds.
+//
+//                  The program also uses an instrumentation
+//                  scheme (implemented in "myDebug.h") that allows one to see
+//                  program events on an oscilloscope or logic analyzer.
+//                  See the "myDebug.h" file for details on how to use this
+//                  important feature.
+//
 //                  The debugging is set up to show the following information:
 //                      PORTE bit 0: Timer2 ISR
 //                      PORTE bit 1: When LED1 is on
@@ -30,8 +43,10 @@ unsigned int timer_count = 0;
 
 
 void __ISR(_TIMER_3_VECTOR, IPL4AUTO) _Timer3Handler(void) {
+    DBG_ON(MASK_DBG0);
     timer_count++; // Increment the millisecond counter.
     INTClearFlag(INT_T3); // Clear the Timer2 interrupt flag.
+     DBG_OFF(MASK_DBG0);
 }
 
 void initTimer() {
@@ -49,19 +64,22 @@ int main() {
     // Initialize PmodOLED
     DelayInit();
     OledInit();
+    DBG_INIT();
 
     initTimer();
+    INTConfigureSystem(INT_SYSTEM_CONFIG_MULT_VECTOR);
+    INTEnableInterrupts();
 
     // Send a welcome message to the OLED display
     OledClearBuffer();
-    OledSetCursor(0, 1); 
+    OledSetCursor(0, 0); 
     OledPutString("Time Elapsed = ");
     OledUpdate();
     while (1){
    
         char buf[17];
         sprintf(buf, "%d seconds", timer_count);
-        OledSetCursor(5,2);
+        OledSetCursor(2,2);
         OledPutString(buf); 
         OledUpdate();
 
@@ -69,7 +87,3 @@ int main() {
 
     }
 }
-
-
-
-
